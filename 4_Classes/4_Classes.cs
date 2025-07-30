1d3332fad0c114c7f5d0865cfb2a8e3631dcb6d6
siberrrian <siberrrian@gmail.com>
2025-07-30 23:50:22 +0300

    Add task Classes

---
+++ 4_Classes/4_Classes.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace _4_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit unit1 = new Unit("Player1");
+            Unit unit2 = new Unit("Player2");
+
+            Console.WriteLine("Unit1 Name = " + unit1.Name);
+            Console.WriteLine("Unit2 Name = " + unit2.Name);
+
+            Console.WriteLine("Unit1 Health = " + unit1.Health);
+            Console.WriteLine("Unit2 Health = " + unit2.Health);
+
+            Console.WriteLine("Unit1 Armour = " + unit1.Armour);
+            Console.WriteLine("Unit2 Armour = " + unit2.Armour);
+
+            Console.WriteLine("Unit1 RealHealth = " + unit1.GetRealHealth());
+            Console.WriteLine("Unit2 RealHealth = " + unit2.GetRealHealth());
+
+            Console.WriteLine("Unit1 Damage = " + unit1.Damage);
+            Console.WriteLine("Unit2 Damage = " + unit2.Damage);
+
+            Console.WriteLine("Unit1 hit Unit2");
+            if (!unit2.SetDamage(unit1.Damage))
+            {
+                Console.WriteLine("Unit2 is alive");
+            }
+            else
+            {
+                Console.WriteLine("Unit2 died");
+            }
+            Console.WriteLine("Unit2 Health = " + unit2.Health);
+            Console.WriteLine("Unit2 RealHealth = " + unit2.GetRealHealth());
+
+            Console.WriteLine("Unit1 hit Unit2 again");
+            if (!unit2.SetDamage(unit1.Damage))
+            {
+                Console.WriteLine("Unit2 is alive");
+            }
+            else
+            {
+                Console.WriteLine("Unit2 died");
+            }
+            Console.WriteLine("Unit2 Health = " + unit2.Health);
+            Console.WriteLine("Unit2 RealHealth = " + unit2.GetRealHealth());
+        }
+    }
+}
---
+++ 4_Classes/Unit.cs
@@ -0,0 +1,49 @@
+﻿namespace _4_Classes
+{
+    internal class Unit
+    {
+        private float _health;
+        private float _armour;
+        public string Name { get; }
+        public float Health => _health;
+        public int Damage { get; }
+
+        public Unit() : this("Unknown unit")
+        {
+
+        }
+
+        public Unit (string name)
+        {
+            Name = name;
+            _health = 100f;
+            Damage = 5;
+            Armour = 0.6f;
+        }
+
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armour);
+        }
+
+        public bool SetDamage(float gettingDamage)
+        {
+            _health = Health - gettingDamage * Armour;
+            return (Health <= 0f);
+        }
+
+        public float Armour
+        {
+            get { return (float) Math.Round(_armour, 2); }
+            set
+            {
+                if (value >= 0f || value <= 1f)
+                {
+                    _armour = value;
+                }
+            }
+
+        }
+
+    }
+}
